7c7763782355eb3d305d16c1e61ef19185c6300c
jashpal00007 <jashpal00007@gmail.com>
2026-08-18 20:54:20 +0530

    Add program to check for right angle triangle

---
+++ Check-right-angle-triangle.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main()
+{
+int a,b,c;
+scanf("%d%d%d",&a,&b,&c);
+    
+    if(a*a+b*b==c*c||a*a+c*c==b*b||c*c+b*b==a*a)
+    {
+        printf("Yes");
+    }
+    else
+    {
+        printf("No");
+    }
+}
